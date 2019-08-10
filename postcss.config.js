8730f4d81accb4dd40d64f7c5e049cff35d8dc9b
Zidane <zidane.wright@speurgroup.com>
2019-08-09 23:39:51 -0500

    add purgecss to reduce build size

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
+const purgecss = require('@fullhuman/postcss-purge-css')({
+  content: [
+    './src/**/*.html',
+    './src/**/*.vue'
+  ],
+
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    purgecss
   ]
 }
